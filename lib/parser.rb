7f91665e8b3948d394c8478851f948ebad41df73
Marcle Rodrigues <maarclee@gmail.com>
2016-04-27 23:48:58 -0300

    Parser Module and Specs

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,5 +1,58 @@
 require "parser/version"
+require "modules/rules"
+require "models/game"
 
 module Parser
-  # Your code goes here...
+  def self.set_game
+    @games = []
+    @current_game = nil
+  end
+
+  def self.parse logfile
+    log = File.new logfile
+    reset_status
+    while line == log.gets
+      process_line line
+    end
+
+  rescue  => error
+    puts "Parse failed while reading the log file. Error: #{error}"
+    raise error
+  end
+
+  def self.reset_status
+    @games.clear
+    @current_game = nil
+  end
+
+  def self.version
+    Parser::Version
+  end
+
+  def self.games
+    @games
+  end
+
+  def self.current_game
+    @current_game
+  end
+
+  def self.process_line line
+    if line =~ Rules.init_game
+      @current_game = Game.new
+      @games.push(@current_game)
+    elsif line =~ Rules.kill
+      parse_kill_line line
+    end
+  end
+
+  def self.parse_kill_line line
+    kill_line = line.split(':').last
+    kill_data = kill_line.split(Rules.kill_split)
+    killer = kill_data[0].strip
+    killed = kill_data[1].strip
+    cause = kill_data[2].strip
+    @current_game.new_kill killer, killed, cause
+  end
+
 end
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -4,4 +4,80 @@ describe Parser do
   it 'has a version number' do
     expect(Parser::VERSION).not_to be nil
   end
+
+  describe 'set game' do
+    before :all do
+      Parser.set_game
+    end
+
+    it 'defines games' do
+      expect(Parser.games).not_to be_nil
+    end
+
+    it 'defines current_game' do
+      expect(Parser.current_game).to be_nil
+    end
+  end
+
+  describe 'reset status' do
+    before :all do
+      Parser.set_game
+      Parser.reset_status
+    end
+
+    it 'resets games' do
+      expect(Parser.games).to eq([])
+    end
+
+    it 'resets current_game' do
+      expect(Parser.current_game).to be_nil
+    end
+  end
+
+  describe 'process line' do
+    context 'InitGame line' do
+      before :all do
+        line = "InitGame"
+        Parser.process_line line
+      end
+
+      it 'sets the current_game' do
+        expect(Parser.current_game).not_to be_nil
+      end
+
+      it 'increases games count' do
+        expect(Parser.games.count).to eq(1)
+      end
+    end
+
+    context 'Kill line' do
+      before :all do
+        line = "12:48 Kill: 3 2 6: Oootsimo killed Isgalamido by MOD_ROCKET"
+        Parser.process_line line
+      end
+
+      it 'creates a new kill' do
+        expect(Parser.current_game.kills.count).to eq(1)
+      end
+    end
+  end
+
+  describe 'Parse kill line' do
+    context 'creates new kill' do
+      before :all do
+        Parser.set_game
+        Parser.process_line "InitGame"
+        line = "12:48 Kill: 3 2 6: Oootsimo killed Isgalamido by MOD_ROCKET"
+        Parser.process_line line
+      end
+
+      it 'sets the killer' do
+        expect(Parser.current_game.players["Oootsimo"].kills).to eq(1)
+      end
+
+      it 'sets the killed' do
+        expect(Parser.current_game.players["Isgalamido"].deaths).to eq(1)
+      end
+    end
+  end
 end
